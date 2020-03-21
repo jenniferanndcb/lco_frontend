400dca0185f2bbda06427c9683de190fa18ad447
Jen <jenniferanndcb@gmail.com>
2020-03-21 21:12:53 +0000

    PostcodeForm render

--- src/components/PostcodeForm.js
+++ src/components/PostcodeForm.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 export default class PostcodeForm extends React.Component {
-
   state = {
     postcode: ""
-  }
+  };
 
- 
+  render() {
+    return (
+      <form onSubmit={this.handleOnSubmit}>
+        <label>Enter Postcode:</label>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleOnChange}
+        />
+      </form>
+    );
+  }
 }
