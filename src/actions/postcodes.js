8463a817c39f337b1d67c120c0433d6c1b3bdec0
Jen <jenniferanndcb@gmail.com>
2020-03-21 22:20:50 +0000

    need to sort out fetch postcode

--- src/actions/postcodes.js
+++ src/actions/postcodes.js
@@ -1,9 +1,10 @@
 //Find Local Authority based on postcode entered
 
 export function findLocalAuthority(postcode) {
+  debugger
   return dispatch => {
     dispatch({ type: "START_FETCHING_LA" });
-    fetch("https://api.postcodes.io/postcodes" + "/" + postcode)
+    fetch("https://api.postcodes.io/postcodes" + "/" + {postcode})
       .then(res => res.json())
       .then(data => dispatch({ type: "FIND_LOCAL_AUTHORITY" }, data));
   };
