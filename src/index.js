6b0d658d3fa11777bde2ebd9654d008102920aa7
Jen <jenniferanndcb@gmail.com>
2020-03-21 22:06:42 +0000

    applyMiddleware and thunk

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers/index'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers/index";
+import thunk from "redux-thunk";
+import * as serviceWorker from "./serviceWorker";
 
-let store = createStore(rootReducer)
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
