1d8c3c9b503bfa9a9a36a61fefb12e013feba216
Jen <jenniferanndcb@gmail.com>
2020-03-21 21:10:10 +0000

    PostcodeForm state

---
+++ src/components/PostcodeForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class PostcodeForm extends React.Component {
+
+  state = {
+    postcode: ""
+  }
+
+ 
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
