d3f2ccf0c9add022155af00d8f2f29e94802cad0
Jen <jenniferanndcb@gmail.com>
2020-03-22 17:27:57 +0000

    installed chartjs2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PostcodeForm from "./components/PostcodeForm";
 import Title from "./components/Title";
+import Charts from "./components/Charts";
 
 function App() {
   return (
     <div className="App">
       <Title />
       <PostcodeForm />
+      <Charts />
     </div>
   );
 }
--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
+import { connect } from "react-redux";
 
 class Charts extends React.Component {
   render() {
