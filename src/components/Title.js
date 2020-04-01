e4ea8cd2497d1e5ca08c97c32e47231377643e1b
Jen <jenniferanndcb@gmail.com>
2020-04-01 22:54:19 +0100

    LA title only if exists

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 
-
 class Title extends React.Component {
   render() {
     return (
       <div>
         <h1>Prevalance of Childhood Obesity in London 2018/19</h1>
-        <h2>London Borough of {this.props.localAuthority}</h2>
+
+        {this.props.localAuthority && (
+          <h2>London Borough of {this.props.localAuthority}</h2>
+        )}
       </div>
     );
   }
