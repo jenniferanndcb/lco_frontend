4bc8b4d4475deea519e88757c68105a0c131baca
Jen <jenniferanndcb@gmail.com>
2020-04-05 22:23:39 +0100

    removed postcodes action reducer

--- src/reducers/londonData.js
+++ src/reducers/londonData.js
@@ -1,20 +1,20 @@
-export default (state = { londonData: "", requesting: false }, action) => {
-  switch (action.type) {
-    case "GET_LONDON_DATA'":
-      return {
-        ...state,
-        londonData: "",
-        requesting: true
-      };
+// export default (state = { londonData: "", requesting: false }, action) => {
+//   switch (action.type) {
+//     case "GET_LONDON_DATA'":
+//       return {
+//         ...state,
+//         londonData: "",
+//         requesting: true
+//       };
 
-    case "SHOW_LONDON_DATA":
-      return {
-        ...state,
-        londonData: action.payload,
-        requesting: false
-      };
+//     case "SHOW_LONDON_DATA":
+//       return {
+//         ...state,
+//         londonData: action.payload,
+//         requesting: false
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
