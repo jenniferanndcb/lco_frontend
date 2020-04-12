5b0af8b5b2064eeb0793012b6b0e5c0a0d83ab87
Jen <jenniferanndcb@gmail.com>
2020-04-12 13:40:31 +0100

    added Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import ChartsContainer from "./containers/ChartsContainer";
 import LondonProgrammesContainer from "./containers/LondonProgrammesContainer";
 
-
 function App() {
   return (
-    <div className="App">
-      <Title />
-      <ChartsContainer />
-      <LondonProgrammesContainer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Title} />
+        <Route exact path="/data" component={ChartsContainer} />
+        <Route exact path="/programmes" component={LondonProgrammesContainer} />
+      </div>
+    </Router>
   );
 }
 
